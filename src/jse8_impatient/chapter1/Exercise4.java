804f8e25eb08da1245a11a5e663c62428f5fc10b
tlanders <tlanders@landersconsulting.biz>
2014-11-22 13:24:33 -0600

    adding sort code

--- src/jse8_impatient/chapter1/Exercise4.java
+++ src/jse8_impatient/chapter1/Exercise4.java
@@ -1,5 +1,9 @@
 package jse8_impatient.chapter1;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by tlanders on 11/22/2014.
  */
@@ -11,5 +15,21 @@ public class Exercise4 {
         }
 
         System.out.println("Exercise4, dir=" + args[0]);
+
+        List<File> files = Arrays.asList(new File(args[0]).listFiles());
+        files.sort((file1, file2) -> {
+            if(file1.isFile() && file2.isFile() || file1.isDirectory() && file2.isDirectory()) {
+                // files are same types so compare their names
+                return file1.getName().compareTo(file2.getName());
+            } else if(file1.isDirectory()) {
+                // types are different, directories are shown first so return -1
+                return -1;
+            } else {
+                // types are different, files are shown last so return 1
+                return 1;
+            }
+        });
+
+        files.forEach(f -> System.out.println((f.isFile() ? "file: " : "dir: ") + f.getName()));
     }
 }
