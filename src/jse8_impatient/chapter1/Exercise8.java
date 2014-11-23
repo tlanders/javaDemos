cc4e9225a370aaa0727fdaa1e5188390735b83f6
tlanders <tlanders@landersconsulting.biz>
2014-11-23 17:06:48 -0600

    start of exercise 8

---
+++ src/jse8_impatient/chapter1/Exercise8.java
@@ -0,0 +1,10 @@
+package jse8_impatient.chapter1;
+
+/**
+ * Created by tlanders on 11/22/2014.
+ */
+public class Exercise8 {
+    public static void main(String [] args) {
+        System.out.println("Exercise8");
+    }
+}
