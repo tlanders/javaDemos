77d0df7fccd717a601cab9491a4292f36bf6784c
tlanders <tlanders@landersconsulting.biz>
2014-11-23 09:50:48 -0600

    main method added

---
+++ src/jse8_impatient/chapter1/Exercise6.java
@@ -0,0 +1,14 @@
+package jse8_impatient.chapter1;
+
+import java.io.File;
+import java.util.Arrays;
+
+/**
+ * Created by tlanders on 11/22/2014.
+ */
+public class Exercise6 {
+    public static void main(String [] args) {
+        System.out.println("Exercise6");
+
+    }
+}
