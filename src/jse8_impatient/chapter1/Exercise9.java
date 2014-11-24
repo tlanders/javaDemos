f7d8afd7191a37fe7629ba6ce74e54dde01b45b1
tlanders <tlanders@landersconsulting.biz>
2014-11-23 19:49:46 -0600

    start of class for exercise 9

---
+++ src/jse8_impatient/chapter1/Exercise9.java
@@ -0,0 +1,13 @@
+package jse8_impatient.chapter1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tlanders on 11/22/2014.
+ */
+public class Exercise9 {
+    public static void main(String [] args) throws Exception {
+        System.out.println("Exercise9");
+    }
+}
