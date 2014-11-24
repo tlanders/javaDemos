ff30bb7ede0bb65144ad2537670ca9e854178865
tlanders <tlanders@landersconsulting.biz>
2014-11-23 20:31:32 -0600

    interface for exercise 9

---
+++ src/jse8_impatient/chapter1/Collection2.java
@@ -0,0 +1,18 @@
+package jse8_impatient.chapter1;
+
+import java.util.Collection;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+/**
+ * Created by tlanders on 11/23/2014.
+ */
+interface Collection2<T> extends Collection<T> {
+    default void forEachIf(Consumer<T> action, Predicate<T> filter) {
+        forEach(obj -> {
+            if(filter.test(obj)) {
+                action.accept(obj);
+            }
+        });
+    }
+}
