fd0d43e492a1c5e852039a48d2d967afaff381ea
tlanders <tlanders@landersconsulting.biz>
2014-11-29 12:10:34 -0600

    finished exercise 1

--- src/jse8_impatient/chapter2/Exercise1.java
+++ src/jse8_impatient/chapter2/Exercise1.java
@@ -6,6 +6,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by tlanders on 11/22/2014.
@@ -38,6 +39,20 @@ public class Exercise1 {
 
         System.out.println("Exercise1, parallel stream count=" + count);
 
+        // write parallel method the hard way with 2 threads and thread-safe counter
+        AtomicLong atomicCounter = new AtomicLong(0);
 
+        List<String> words1 = words.subList(0, words.size() / 2);
+        List<String> words2 = words.subList(words.size() / 2, words.size());
+
+        new Thread(() -> {
+            words1.stream().filter(w -> w.length() > 12).forEach(x -> atomicCounter.incrementAndGet());
+        }).start();
+        new Thread(() -> {
+            words2.stream().filter(w -> w.length() > 12).forEach(x -> atomicCounter.incrementAndGet());
+        }).start();
+
+        Thread.sleep(2000); // let threads exit
+        System.out.println("Exercise1, parallel for count=" + atomicCounter.get());
     }
 }
