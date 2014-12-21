8bc8337b9f5cfa2716bbf5dcbaa2b3679c3a1b9f
tlanders <tlanders@landersconsulting.biz>
2014-12-21 17:24:07 -0600

    finished chapter 2 exercise 2

---
+++ src/jse8_impatient/chapter2/Exercise2.java
@@ -0,0 +1,29 @@
+package jse8_impatient.chapter2;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by tlanders.
+ */
+public class Exercise2 {
+    public static void main(String [] args) throws Exception {
+        System.out.println("Exercise2");
+
+        if (args.length < 1) {
+            System.out.println("USAGE: java Exercise2 INPUT_FILE");
+            System.exit(1);
+        }
+
+        String contents = new String(Files.readAllBytes(Paths.get(args[0])), StandardCharsets.UTF_8); // Read file into string
+        List<String> words = Arrays.asList(contents.split("[\\P{L}]+")); // Split into words; nonletters are delimiters
+
+        // filter with limit of 5
+        long count = words.stream().filter(w -> w.length() > 12).peek(e -> System.out.println("filter called, word=" + e)).limit(5).count();
+
+        System.out.println("Exercise1, filter limit of 5 stream count=" + count);
+    }
+}
