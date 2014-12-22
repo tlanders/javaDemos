62b78e214e74d69a5315a323623640b4cf7985ce
tlanders <tlanders@landersconsulting.biz>
2014-12-21 23:02:05 -0600

    start of exercise 8

--- src/jse8_impatient/chapter2/Exercise8.java
+++ src/jse8_impatient/chapter2/Exercise8.java
@@ -7,22 +7,8 @@ import java.util.stream.Stream;
  */
 public class Exercise8 {
     public static void main(String [] args) throws Exception {
-        System.out.println("Chapter2 - Exercise7");
-
-        boolean result = isFinite(Stream.of(1, 2, 3, 4, 5));
-        System.out.println("Chapter2 - Exercise7, isFinite=" + result);
+        System.out.println("Chapter2 - Exercise8");
 
     }
 
-    /**
-     * This method is a bad idea because you don't know that a stream is finite until it ends. An infinite
-     * stream will cause this method to never exit.
-     * @param stream
-     * @param <T>
-     * @return
-     */
-    public static <T> boolean isFinite(Stream<T> stream) {
-        stream.count();
-        return true;
-    }
 }
