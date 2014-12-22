cf7a9bf577a6e336a1658a8b6ccee1fd12929a68
tlanders <tlanders@landersconsulting.biz>
2014-12-21 18:29:54 -0600

    finished ch2 exercise 5

--- src/jse8_impatient/chapter2/Exercise5.java
+++ src/jse8_impatient/chapter2/Exercise5.java
@@ -10,5 +10,10 @@ public class Exercise5 {
     public static void main(String [] args) throws Exception {
         System.out.println("Chapter2 - Exercise5");
 
+        randomGenerator(25214903917L, 11L, (long) Math.pow(2,48), System.currentTimeMillis()).limit(10).forEach(System.out::println);
+    }
+
+    public static Stream<Long> randomGenerator(long a, long c, long m, long seed) {
+        return Stream.iterate(seed, x -> (a * x + c) % m);
     }
 }
