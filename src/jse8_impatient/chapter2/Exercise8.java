85a56f385db9a3f5b97cbc4cfc1915c320954dc8
tlanders <tlanders@landersconsulting.biz>
2014-12-21 23:23:39 -0600

    finished exercise 8

--- src/jse8_impatient/chapter2/Exercise8.java
+++ src/jse8_impatient/chapter2/Exercise8.java
@@ -1,5 +1,9 @@
 package jse8_impatient.chapter2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.stream.Stream;
 
 /**
@@ -9,6 +13,19 @@ public class Exercise8 {
     public static void main(String [] args) throws Exception {
         System.out.println("Chapter2 - Exercise8");
 
+        zip(Stream.of("a", "b", "c", "d", "e"), Stream.of("1", "2", "3", "4")).forEach(System.out::println);
     }
 
+    public static <T> Stream<T> zip(Stream<T> s1, Stream<T> s2) {
+        Iterator<T> s2iter = s2.iterator();
+        return s1.flatMap(s1obj -> {
+                    if(s2iter.hasNext()) {
+                        return Arrays.asList(s1obj, s2iter.next()).stream();
+                    } else {
+                        s1.close();
+                        return null;
+                    }
+                }
+        );
+    }
 }
