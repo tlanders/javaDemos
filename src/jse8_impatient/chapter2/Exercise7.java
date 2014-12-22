cb0106cfb123add5bffe5d72f43d2ae902df5814
tlanders <tlanders@landersconsulting.biz>
2014-12-21 22:56:58 -0600

    finished ch2 exercise 7

--- src/jse8_impatient/chapter2/Exercise7.java
+++ src/jse8_impatient/chapter2/Exercise7.java
@@ -15,6 +15,13 @@ public class Exercise7 {
 
     }
 
+    /**
+     * This method is a bad idea because you don't know that a stream is finite until it ends. An infinite
+     * stream will cause this method to never exit.
+     * @param stream
+     * @param <T>
+     * @return
+     */
     public static <T> boolean isFinite(Stream<T> stream) {
         stream.count();
         return true;
