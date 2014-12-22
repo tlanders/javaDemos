974896ebbd56290f312d3f51283dc38cad3f8d5f
tlanders <tlanders@landersconsulting.biz>
2014-12-21 18:07:00 -0600

    start of exercise 5

---
+++ src/jse8_impatient/chapter2/Exercise5.java
@@ -0,0 +1,14 @@
+package jse8_impatient.chapter2;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+/**
+ * Created by tlanders.
+ */
+public class Exercise5 {
+    public static void main(String [] args) throws Exception {
+        System.out.println("Chapter2 - Exercise5");
+
+    }
+}
