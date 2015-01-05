2baa39e43d03b251e9ee8398a1a23b4fd6fa7d14
tlanders <tlanders@landersconsulting.biz>
2015-01-05 12:20:12 -0600

    done with first reduce method

--- src/jse8_impatient/chapter2/Exercise9.java
+++ src/jse8_impatient/chapter2/Exercise9.java
@@ -1,8 +1,7 @@
 package jse8_impatient.chapter2;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
@@ -12,10 +11,32 @@ public class Exercise9 {
     public static void main(String [] args) throws Exception {
         System.out.println("Chapter2 - Exercise9");
 
+        ArrayList<Integer> a1 = new ArrayList<>();
+        a1.add(1);
+        a1.add(2);
+        a1.add(3);
 
+        ArrayList<Integer> a2 = new ArrayList<>();
+        a2.add(99);
+        a2.add(88);
+        a2.add(77);
+        a2.add(66);
+
+        System.out.println("  a1 size=" + a1.size());
+        System.out.println("  a2 size=" + a2.size());
+
+        ArrayList<Integer> afinal = join1(Stream.of(a1, a2));
+
+        System.out.println("Chapter2 - Exercise9, joined size=" + afinal.size());
+        afinal.forEach(System.out::println);
     }
 
-    public static <T> Stream<T> join(Stream<ArrayList<T>> s1) {
-        return Stream.empty();
+    public static <T> ArrayList<T> join1(Stream<ArrayList<T>> s1) {
+        Optional<ArrayList<T>> r = s1.reduce((a1, a2) -> {
+            ArrayList<T> a = new ArrayList<T>(a1);
+            a.addAll(a2);
+            return a;
+        });
+        return r.get();
     }
 }
