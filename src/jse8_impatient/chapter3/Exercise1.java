03519bf73863040d33e365720024267d6c738884
tlanders <tlanders@landersconsulting.biz>
2015-01-25 18:07:03 -0600

    start of exercise 1

---
+++ src/jse8_impatient/chapter3/Exercise1.java
@@ -0,0 +1,19 @@
+package jse8_impatient.chapter3;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by tlanders on 11/22/2014.
+ */
+public class Exercise1 {
+    public static void main(String [] args) throws Exception {
+        System.out.println("Exercise1");
+
+        System.out.println("Exercise1, done");
+    }
+}
