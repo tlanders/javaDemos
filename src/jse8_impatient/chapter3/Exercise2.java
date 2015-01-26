e9863f0cc6d92a8c964f5e6d89f26c7717e8aca8
tlanders <tlanders@landersconsulting.biz>
2015-01-25 18:32:13 -0600

    finished exercise 2

--- src/jse8_impatient/chapter3/Exercise2.java
+++ src/jse8_impatient/chapter3/Exercise2.java
@@ -1,5 +1,6 @@
 package jse8_impatient.chapter3;
 
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.BooleanSupplier;
 import java.util.function.Supplier;
 import java.util.logging.Level;
@@ -15,7 +16,17 @@ public class Exercise2 {
     public static void main(String [] args) throws Exception {
         System.out.println("Exercise2");
 
+        withLock(new ReentrantLock(), () -> System.out.println("printing with lock"));
 
         System.out.println("Exercise2, done");
+    }
+
+    public static void withLock(ReentrantLock myLock, Runnable action) {
+        myLock.lock();
+        try {
+            action.run();
+        } finally {
+            myLock.unlock();
+        }
     }
 }
