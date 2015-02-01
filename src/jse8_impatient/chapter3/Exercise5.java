ef50e5e73f85bcadd05e8c5eca15e7c61d477047
tlanders <tlanders@landersconsulting.biz>
2015-02-01 16:45:39 -0600

    start of exercise 5

---
+++ src/jse8_impatient/chapter3/Exercise5.java
@@ -0,0 +1,17 @@
+package jse8_impatient.chapter3;
+
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Logger;
+
+/**
+ * Created by tlanders.
+ */
+public class Exercise5 {
+    private static Logger logger = Logger.getLogger(Exercise5.class.getName());
+
+    public static void main(String [] args) throws Exception {
+        System.out.println("Exercise5");
+
+        System.out.println("Exercise5, done");
+    }
+}
