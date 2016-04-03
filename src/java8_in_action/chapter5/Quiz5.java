410d49100b6a85c0e4a6a6a46fc58b0eb90ab121
tlanders <tlanders@landersconsulting.biz>
2016-04-02 22:32:36 -0500

    done with question 2

--- src/java8_in_action/chapter5/Quiz5.java
+++ src/java8_in_action/chapter5/Quiz5.java
@@ -12,6 +12,7 @@ public class Quiz5 {
         System.out.println("Quiz 5");
 
         question1();
+        question2();
 
         System.out.println("Quiz 5 done.");
     }
@@ -23,10 +24,21 @@ public class Quiz5 {
         squares.forEach(System.out::println);
     }
 
+    /**
+     * Takes two arrays and outputs the pairs of all numbers in the two lists.
+     * Example: (1,2) + (3,4) -> (1,3), (1,4), (2,3), (2,4)
+     */
     public static void question2() {
         System.out.println("question 2");
         //List<Integer> squares = Arrays.asList(1, 2, 3, 4, 5).stream().map(n -> n * n).collect(Collectors.toList());
 
-        ///squares.forEach(System.out::println);
+        List<Integer> first = Arrays.asList(1,2,3);
+        List<Integer> second = Arrays.asList(4,5);
+
+        List<int []> pairs = first.stream()
+                .flatMap(i -> second.stream().map(j -> new int[] {i, j}))
+                .collect(Collectors.toList());
+
+        pairs.stream().forEach(pair -> System.out.println(pair[0] + ":" + pair[1]));
     }
 }
