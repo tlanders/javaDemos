54fb6daba6948ed9243a515708ffdd30e4c87b0c
tlanders <tlanders@landersconsulting.biz>
2016-04-06 20:36:03 -0500

    start of quiz 5.3

---
+++ src/java8_in_action/chapter5/Quiz5_3.java
@@ -0,0 +1,13 @@
+package java8_in_action.chapter5;
+
+/**
+ * Created by tlanders on 4/6/2016.
+ */
+public class Quiz5_3 {
+    public static void main(String [] args) {
+        System.out.println("Quiz 5_3");
+
+
+        System.out.println("Quiz 5_3 done.");
+    }
+}
