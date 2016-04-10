a13a87ee5ea94ffd52355d4d906fe2d56970ce8d
tlanders <tlanders@landersconsulting.biz>
2016-04-10 18:00:59 -0500

    finished generate exercises

--- src/java8_in_action/chapter5/Chapter5_7.java
+++ src/java8_in_action/chapter5/Chapter5_7.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.IntSupplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -28,6 +29,26 @@ public class Chapter5_7 {
         } catch(IOException ioe) {
             ioe.printStackTrace();
         }
+
+        // generating first 20 fibonacci tuples with iterate
+        Stream.iterate(new int[] {0,1}, n -> new int[] {n[1], n[0] + n[1]})
+                .limit(20)
+                .forEach(n-> System.out.println("(" + n[0] + "," + n[1] + ")"));
+
+        // generating first 20 fibonaccci values with stateful generate (generally a bad thing because use in
+        // multiple threads)
+        IntStream.generate(new IntSupplier() {
+            private int v1 = 0;
+            private int v2 = 1;
+            @Override
+            public int getAsInt() {
+                int tempVal = v1;
+                v1 = v2;
+                v2 = v1 + tempVal;
+                return tempVal;
+            }
+        }).limit(20).forEach(System.out::println);
+
         System.out.println("Chapter 5_7 done.");
     }
 }
