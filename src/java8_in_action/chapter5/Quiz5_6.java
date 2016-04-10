e6257d29327b1d06fd84909066fb203a8102bf6a
tlanders <tlanders@landersconsulting.biz>
2016-04-09 21:32:58 -0500

    start of 5.6 quiz

---
+++ src/java8_in_action/chapter5/Quiz5_6.java
@@ -0,0 +1,17 @@
+package java8_in_action.chapter5;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by tlanders on 4/6/2016.
+ */
+public class Quiz5_6 {
+    public static void main(String [] args) {
+        System.out.println("Quiz 5_6");
+
+        System.out.println("Quiz 5_6 done.");
+    }
+}
