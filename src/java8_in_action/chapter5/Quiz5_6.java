06c6acace58df305014b419dde244a012be8e42a
tlanders <tlanders@landersconsulting.biz>
2016-04-09 21:40:31 -0500

    half of pythagorean tuple search done

--- src/java8_in_action/chapter5/Quiz5_6.java
+++ src/java8_in_action/chapter5/Quiz5_6.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Created by tlanders on 4/6/2016.
@@ -11,6 +12,16 @@ import java.util.stream.Collectors;
 public class Quiz5_6 {
     public static void main(String [] args) {
         System.out.println("Quiz 5_6");
+
+        int a = 6;
+
+        List<int[]> tuples = IntStream.rangeClosed(1, 100)
+                .boxed()
+                .filter(b -> Math.sqrt(a*a + b*b) % 1.0 == 0)
+                .map(b -> new int[] {a, b, (int) Math.sqrt(a*a + b*b)})
+                .collect(Collectors.toList());
+
+        tuples.stream().forEach(t -> System.out.println("[" + t[0] + "," + t[1] + "," + t[2] + "]"));
 
         System.out.println("Quiz 5_6 done.");
     }
