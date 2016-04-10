dbbc369ac9fe173209deaeefaa43cec84b86faea
tlanders <tlanders@landersconsulting.biz>
2016-04-10 13:54:18 -0500

    finished pythagorean tuple search

--- src/java8_in_action/chapter5/Quiz5_6.java
+++ src/java8_in_action/chapter5/Quiz5_6.java
@@ -13,15 +13,28 @@ public class Quiz5_6 {
     public static void main(String [] args) {
         System.out.println("Quiz 5_6");
 
-        int a = 6;
-
+        // calculates Pythagorean triples
         List<int[]> tuples = IntStream.rangeClosed(1, 100)
                 .boxed()
-                .filter(b -> Math.sqrt(a*a + b*b) % 1.0 == 0)
-                .map(b -> new int[] {a, b, (int) Math.sqrt(a*a + b*b)})
+                .flatMap(a -> IntStream.rangeClosed(a, 100)
+                    .boxed()
+                    .filter(b -> Math.sqrt(a*a + b*b) % 1.0 == 0)
+                    .map(b -> new int[] {a, b, (int) Math.sqrt(a*a + b*b)}))
                 .collect(Collectors.toList());
 
         tuples.stream().forEach(t -> System.out.println("[" + t[0] + "," + t[1] + "," + t[2] + "]"));
+
+        System.out.println("Quiz 5_6 - method 2");
+        // calculates Pythagorean triples
+        List<double[]> tuples2 = IntStream.rangeClosed(1, 100)
+                .boxed()
+                .flatMap(a -> IntStream.rangeClosed(a, 100)
+                        .boxed()
+                        .map(b -> new double[] {a , b, Math.sqrt(a*a + b*b)})
+                        .filter(x -> x[2] % 1.0 == 0))
+                .collect(Collectors.toList());
+
+        tuples2.stream().forEach(t -> System.out.println("[" + (int) t[0] + "," + (int) t[1] + "," + (int) t[2] + "]"));
 
         System.out.println("Quiz 5_6 done.");
     }
