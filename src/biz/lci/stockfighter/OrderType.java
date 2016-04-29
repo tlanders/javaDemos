38a863eaba1e3d7eeeae458461cfe040b9ddac12
tlanders <tlanders@landersconsulting.biz>
2016-04-29 18:59:21 -0500

    added last order type

--- src/biz/lci/stockfighter/OrderType.java
+++ src/biz/lci/stockfighter/OrderType.java
@@ -11,7 +11,9 @@ import java.util.Arrays;
 public enum OrderType {
     limit("limit"),
     market("market"),
-    immediate_or_cancel("immediate-or-cancel");
+    fill_or_kill("fill-or-kill"),   // limit order that is either immediately fully filled or canceled
+    immediate_or_cancel("immediate-or-cancel"); // limit order that is immediately completely or partially filled
+                                                // and then the remainder is canceled
 
     private String value;
 
