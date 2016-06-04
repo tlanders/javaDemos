318c989335b445e8f207f5bc82de4fdb7a64208b
tlanders <tlanders@landersconsulting.biz>
2016-06-04 00:03:50 -0500

    start of nodes challenge

---
+++ src/misc/nodes/Nodes.java
@@ -0,0 +1,12 @@
+package misc.nodes;
+
+/**
+ * Created by tlanders on 6/4/2016.
+ */
+public class Nodes {
+    public static void main(String [] args) {
+        System.out.println("Nodes starting...");
+
+        System.out.println("Nodes done.");
+    }
+}
