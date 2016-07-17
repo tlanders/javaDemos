2effcbc378319fa23dbc1cde4f7d80f8e72d9fac
tlanders <tlanders@landersconsulting.biz>
2016-07-17 18:23:11 -0500

    playing tiles

--- src/misc/scrabble/Scrabble.java
+++ src/misc/scrabble/Scrabble.java
@@ -1,5 +1,7 @@
 package misc.scrabble;
 
+import java.util.stream.IntStream;
+
 /**
  * Solution to Scrabble Sets problem.
  * https://dzone.com/articles/java-code-challenge-scrabble-sets
@@ -8,6 +10,27 @@ public class Scrabble {
     public static void main(String [] args) throws Exception {
         System.out.println("Scrabble starting...");
 
+        try {
+            playTiles("AEERTYOXMCNB_S");
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+
         System.out.println("Scrabble done.");
+    }
+
+    protected static void playTiles(String tiles) throws Exception {
+        ScrabbleState gameState = new ScrabbleState();
+        System.out.print(gameState);
+//        System.out.println("playing tiles=" + tiles);
+
+        for(int i = 0; i < tiles.length(); i++) {
+            char tile = tiles.charAt(i);
+            System.out.println("playing tile=" + tile);
+
+            gameState.playTile(tile);
+        }
+        System.out.println("done playing tiles");
+        System.out.print(gameState);
     }
 }
--- src/misc/scrabble/ScrabbleState.java
+++ src/misc/scrabble/ScrabbleState.java
@@ -2,44 +2,66 @@ package misc.scrabble;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by tlanders on 7/17/2016.
  */
 public class ScrabbleState {
-    protected Map<String, Integer> unplayedTiles = new HashMap<>();
+    protected Map<Character, Integer> unplayedTiles = new TreeMap<>();
 
     public ScrabbleState() {
         initTiles();
     }
 
+    public void playTile(char tile) throws Exception {
+        Integer tileCount = unplayedTiles.get(tile);
+        if(tileCount > 0) {
+            unplayedTiles.put(tile, tileCount - 1);
+        } else {
+            System.out.println("Invalid input. More " + tile + "'s have been taken from the bag than possible.");
+            throw new Exception();
+        }
+    }
+
     protected void initTiles() {
-        unplayedTiles.put("A", 9);
-        unplayedTiles.put("B", 2);
-        unplayedTiles.put("C", 2);
-        unplayedTiles.put("D", 4);
-        unplayedTiles.put("E", 12);
-        unplayedTiles.put("F", 2);
-        unplayedTiles.put("G", 3);
-        unplayedTiles.put("H", 2);
-        unplayedTiles.put("I", 9);
-        unplayedTiles.put("J", 1);
-        unplayedTiles.put("K", 1);
-        unplayedTiles.put("L", 4);
-        unplayedTiles.put("M", 2);
-        unplayedTiles.put("N", 6);
-        unplayedTiles.put("O", 8);
-        unplayedTiles.put("P", 2);
-        unplayedTiles.put("Q", 1);
-        unplayedTiles.put("R", 6);
-        unplayedTiles.put("S", 4);
-        unplayedTiles.put("T", 6);
-        unplayedTiles.put("U", 4);
-        unplayedTiles.put("V", 2);
-        unplayedTiles.put("W", 2);
-        unplayedTiles.put("X", 1);
-        unplayedTiles.put("Y", 2);
-        unplayedTiles.put("Z", 1);
-        unplayedTiles.put("_", 2);
+        unplayedTiles.put('A', 9);
+        unplayedTiles.put('B', 2);
+        unplayedTiles.put('C', 2);
+        unplayedTiles.put('D', 4);
+        unplayedTiles.put('E', 12);
+        unplayedTiles.put('F', 2);
+        unplayedTiles.put('G', 3);
+        unplayedTiles.put('H', 2);
+        unplayedTiles.put('I', 9);
+        unplayedTiles.put('J', 1);
+        unplayedTiles.put('K', 1);
+        unplayedTiles.put('L', 4);
+        unplayedTiles.put('M', 2);
+        unplayedTiles.put('N', 6);
+        unplayedTiles.put('O', 8);
+        unplayedTiles.put('P', 2);
+        unplayedTiles.put('Q', 1);
+        unplayedTiles.put('R', 6);
+        unplayedTiles.put('S', 4);
+        unplayedTiles.put('T', 6);
+        unplayedTiles.put('U', 4);
+        unplayedTiles.put('V', 2);
+        unplayedTiles.put('W', 2);
+        unplayedTiles.put('X', 1);
+        unplayedTiles.put('Y', 2);
+        unplayedTiles.put('Z', 1);
+        unplayedTiles.put('_', 2);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+
+        str.append("Game state:\n");
+        for(char tile : unplayedTiles.keySet()) {
+            str.append(tile).append(" : ").append(unplayedTiles.get(tile)).append('\n');
+        }
+        return str.toString();
     }
 }
