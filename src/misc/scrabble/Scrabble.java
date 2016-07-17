59b55e502d7b13d5033df3cd9b9c8bba08b3d4b6
tlanders <tlanders@landersconsulting.biz>
2016-07-17 17:51:50 -0500

    start of scrabble sets solution

---
+++ src/misc/scrabble/Scrabble.java
@@ -0,0 +1,13 @@
+package misc.scrabble;
+
+/**
+ * Solution to Scrabble Sets problem.
+ * https://dzone.com/articles/java-code-challenge-scrabble-sets
+ */
+public class Scrabble {
+    public static void main(String [] args) throws Exception {
+        System.out.println("Scrabble starting...");
+
+        System.out.println("Scrabble done.");
+    }
+}
