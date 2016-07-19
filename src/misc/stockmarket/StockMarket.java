5075ec4348dcaf6cef0e2fa6123dd535946b00ed
tlanders <tlanders@landersconsulting.biz>
2016-07-18 22:00:55 -0500

    start of stock market

---
+++ src/misc/stockmarket/StockMarket.java
@@ -0,0 +1,13 @@
+package misc.stockmarket;
+
+/**
+ * Solution to stock market (buy low - sell high) problem.
+ * https://dzone.com/articles/java-code-challenge-play-the-stock-market
+ */
+public class StockMarket {
+     public static void main(String [] args) throws Exception {
+         System.out.println("Stock market starting...");
+
+         System.out.println("Stock market done.");
+    }
+}
