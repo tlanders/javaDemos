2f3b3ce50d74b389ce727dca3a8af6d52c2adc54
tlanders <tlanders@landersconsulting.biz>
2016-09-10 16:04:12 -0500

    start of part 1

---
+++ src/misc/periodic_table/Part1.java
@@ -0,0 +1,11 @@
+package misc.periodic_table;
+
+/**
+ * Created by tlanders on 9/10/2016.
+ */
+public class Part1 {
+    public static void main(String [] args) {
+        System.out.println("Part1 starting...");
+        System.out.println("Part1 done.");
+    }
+}
