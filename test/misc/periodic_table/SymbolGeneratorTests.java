d003f4a800b7d6c8d375e685960fed0a08efcf6a
tlanders <tlanders@landersconsulting.biz>
2016-09-11 14:12:57 -0500

    basic 2 char symbol tests

--- test/misc/periodic_table/SymbolGeneratorTests.java
+++ test/misc/periodic_table/SymbolGeneratorTests.java
@@ -1,7 +1,17 @@
 package misc.periodic_table;
 
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
 /**
  * Created by tlanders on 9/11/2016.
  */
 public class SymbolGeneratorTests {
+    @Test
+    public void testValidCheck() {
+        SymbolGenerator sgen = new SymbolGenerator();
+        assertFalse("two char test failed", sgen.isValidSymbol("Element", "E"));
+        assertFalse("two char test failed", sgen.isValidSymbol("Element", "Ele"));
+    }
 }
