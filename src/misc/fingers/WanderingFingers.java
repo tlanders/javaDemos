f1af7d4763c3aad5b63499c5044de2ba39da9c71
tlanders <tlanders@landersconsulting.biz>
2016-11-27 17:19:16 -0600

    start of wandering fingers solution

---
+++ src/misc/fingers/WanderingFingers.java
@@ -0,0 +1,14 @@
+package misc.fingers;
+
+/**
+ * Solution to https://dzone.com/articles/java-code-challenge-wandering-fingers.
+ *
+ * Created by tlanders on 11/27/2016.
+ */
+public class WanderingFingers {
+    public static void main(String [] args) {
+        System.out.println("WanderingFingers starting...");
+
+        System.out.println("WanderingFingers done.");
+    }
+}
