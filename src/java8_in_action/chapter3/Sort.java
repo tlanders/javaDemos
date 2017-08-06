b7729f2ab8dd746f4f20595eb85e2133bf627643
tlanders <tlanders@landersconsulting.biz>
2017-08-06 17:20:38 -0500

    added thenComparing sorts

--- src/java8_in_action/chapter3/Sort.java
+++ src/java8_in_action/chapter3/Sort.java
@@ -18,7 +18,9 @@ public class Sort {
         List<Apple> apples = new ArrayList<>();
         apples.add(new Apple("green", 121));
         apples.add(new Apple("red", 101));
+        apples.add(new Apple("green", 101));
         apples.add(new Apple("pink", 112));
+        apples.add(new Apple("blue", 112));
         apples.add(new Apple("yellow", 95));
         System.out.println("unsorted, apples=" + apples);
 
@@ -43,9 +45,23 @@ public class Sort {
         apples.sort(Comparator.comparing((Apple a) -> a.getWeight()).reversed());
         System.out.println("reverse sorted by comparing and lambda, apples=" + apples);
 
-        // reverse sort with comparing and method reference
+        // sort with comparing and method reference
         apples.sort(Comparator.comparing(Apple::getWeight));
         System.out.println("sorted by comparing and method reference, apples=" + apples);
+
+        // sort with comparing, reversed, and thenComparing using method references
+        // reverses first sort order but not second
+        apples.sort(Comparator.comparing(Apple::getWeight)
+                .reversed()
+                .thenComparing(Apple::getColor));
+        System.out.println("sorted by comparing and thenComparing and method reference, apples=" + apples);
+
+        // sort with comparing, thenComparing, and reversed using method references
+        // reverses both sort orders
+        apples.sort(Comparator.comparing(Apple::getWeight)
+                .thenComparing(Apple::getColor)
+                .reversed());
+        System.out.println("sorted by comparing and thenComparing and method reference, apples=" + apples);
 
         System.out.println("Sort done.");
     }
