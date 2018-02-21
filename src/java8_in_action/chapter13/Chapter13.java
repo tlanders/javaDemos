ad2f984a04cfc1b061080692947648badbe39eb8
tlanders <tlanders@landersconsulting.biz>
2018-02-20 21:16:38 -0600

    class for chapter 13

---
+++ src/java8_in_action/chapter13/Chapter13.java
@@ -0,0 +1,9 @@
+package java8_in_action.chapter13;
+
+public class Chapter13 {
+	public static void main(String [] args) {
+		System.out.println("Chapter 13...");
+		System.out.println("Chapter 13 done.");
+	}
+
+}
