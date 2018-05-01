6c23e06b21ddaa660766a5741701b6e28dc7de8c
tlanders <tlanders@landersconsulting.biz>
2018-04-30 23:32:44 -0500

    enhanced simple stack

--- src/effectivejava/chapter5/Stack.java
+++ src/effectivejava/chapter5/Stack.java
@@ -1,8 +1,12 @@
 package effectivejava.chapter5;
 
+import java.util.Arrays;
+import java.util.EmptyStackException;
+import java.util.Objects;
+
 public class Stack {
 	private Object [] items;
-	private static final int DEFAULT_CAPACITY = 16;
+	private static final int DEFAULT_CAPACITY = 3;
 	private int index = 0;
 	
 	public Stack(int initialCapacity) {
@@ -18,12 +22,25 @@ public class Stack {
 	}
 	
 	public Object pop() {
+		if(index <= 0) {
+			throw new EmptyStackException();
+		}
+		
 		Object item = items[--index];
 		items[index] = null;
 		return item;
 	}
 	
 	public void push(Object obj) {
+		Objects.requireNonNull(obj);
+		
+		ensureCapacity();
 		items[index++] = obj;
+	}
+	
+	private void ensureCapacity() {
+		if(index >= items.length) {
+			items = Arrays.copyOf(items, items.length * 2 + 1);
+		}
 	}
 }
--- src/effectivejava/chapter5/StackTest.java
+++ src/effectivejava/chapter5/StackTest.java
@@ -1,6 +1,9 @@
 package effectivejava.chapter5;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.EmptyStackException;
 
 import org.junit.Test;
 
@@ -10,15 +13,38 @@ public class StackTest {
 	public void testStack() {
 		Stack s = new Stack();
 
+//		List<Object> items = Arrays.asList(1,"a",2,"b",3,"c",4,"d",5,"e",6,"f");
+		
 		s.push(1);
 		s.push("a");
 		s.push(3);
 		s.push("b");
+		s.push(4);
+		s.push("c");
+		s.push(5);
+		s.push("d");
 		
+		assertEquals("d", s.pop());
+		assertEquals(5, s.pop());
+		assertEquals("c", s.pop());
+		assertEquals(4, s.pop());
 		assertEquals("b", s.pop());
 		assertEquals(3, s.pop());
 		assertEquals("a", s.pop());
 		assertEquals(1, s.pop());
+		
+		try {
+			s.pop();
+		} catch(Exception e) {
+			assertTrue(e instanceof EmptyStackException);
+		}
+		
+		try {
+			s.push(null);
+		} catch(Exception e) {
+			assertTrue(e instanceof NullPointerException);
+		}
+		
 	}
 
 }
