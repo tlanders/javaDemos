5fae33deb50cb37ee947404467f6e666adfe87c7
tlanders <tlanders@landersconsulting.biz>
2018-06-02 10:55:08 -0500

    test for weights on planets

--- test/effectivejava/chapter6/EnumTests.java
+++ test/effectivejava/chapter6/EnumTests.java
@@ -15,5 +15,13 @@ public class EnumTests {
 		
 		assertTrue(j.mass() > e.mass());
 		assertTrue(j.surfaceWeight(100.0) > 0);
+		
+		System.out.println("surface weight of 100 kg");
+		for(Planet x : Planet.values()) {
+			final double w = x.surfaceWeight(100.0);
+			assertTrue(w > 0);
+			System.out.println(x.name() + ": weight (N)=" + w 
+				+ ", weight(lbs)=" + (w * .22));
+		}
 	}
 }
