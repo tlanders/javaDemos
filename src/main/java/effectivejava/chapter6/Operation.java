b55eed06922765574beeff7a5943c843cfd8d6b9
tlanders <tlanders@landersconsulting.biz>
2018-06-03 17:23:08 -0500

    added fromSymbol method

--- src/main/java/effectivejava/chapter6/Operation.java
+++ src/main/java/effectivejava/chapter6/Operation.java
@@ -1,5 +1,9 @@
 package effectivejava.chapter6;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 public enum Operation {
 	ADD("+") { 
 		@Override
@@ -33,4 +37,15 @@ public enum Operation {
 	}
 	
 	abstract public double apply(double term1, double term2);
+	
+	final static private Map<String,Operation> OP_SYMBOL_MAP = new HashMap<>();
+	
+	static {
+		for(Operation op : Operation.values()) {
+			OP_SYMBOL_MAP.put(op.toString(), op);
+		}
+	}
+	public static Optional<Operation> fromSymbol(String opSymbol) {
+		return Optional.ofNullable(OP_SYMBOL_MAP.get(opSymbol));
+	}
 }
--- src/test/java/effectivejava/chapter6/EnumTests.java
+++ src/test/java/effectivejava/chapter6/EnumTests.java
@@ -33,5 +33,21 @@ public class EnumTests {
 		assertEquals(-1, Operation.SUBTRACT.apply(2, 3), .0001);
 		assertEquals(6, Operation.MULTIPLY.apply(2, 3), .0001);
 		assertEquals(.6666666, Operation.DIVIDE.apply(2, 3), .0001);
+		
+		assertEquals(Operation.ADD, Operation.valueOf("ADD"));
+		
+		double a = 1.23;
+		double b = -2.34;
+		for(Operation op : Operation.values()) {
+			System.out.printf("%f %s %f = %f\n", a, op, b, op.apply(a, b));
+		}
+		
+		for(Operation op : Operation.values()) {
+			assertEquals(op, Operation.fromSymbol(op.toString()).get());
+			assertEquals(op, Operation.valueOf(op.name()));
+			
+			// fromSymbol only looks up by the symbol and not the operation name
+			assertFalse(Operation.fromSymbol(op.name()).isPresent());
+		}
 	}
 }
