99a96a2493f5255016afa87dcaade04e760dbd83
tlanders <tlanders@landersconsulting.biz>
2018-06-15 20:33:52 -0500

    added search for top 10 frequencies

--- src/main/java/effectivejava/chapter7/StreamsExamples.java
+++ src/main/java/effectivejava/chapter7/StreamsExamples.java
@@ -1,16 +1,30 @@
 package effectivejava.chapter7;
 
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+
 
 public class StreamsExamples {
 	public static void main(String [] args) throws Exception {
-		if(args.length >= 1) {
-			Files.lines(Paths.get(args[0])).collect(Collectors.groupingBy(String::toLowerCase, Collectors.counting()))
-				.forEach((k,v) -> System.out.printf("k: %s, v: %d\n", k, v));
-		} else {
+		if(args.length < 1) {
 			System.out.println("ERROR: please provide word file name.");
 		}
+		
+		Map<String, Long> freqMap = Files.lines(Paths.get(args[0])).collect(Collectors.groupingBy(String::toLowerCase, Collectors.counting()));
+		System.out.println(freqMap);
+		
+		List<String> topKeys = freqMap.keySet().stream()
+				.sorted(comparing(freqMap::get).reversed())
+				.limit(10)
+				.collect(toList());
+		
+		topKeys.forEach(System.out::println);
 	}
 }
