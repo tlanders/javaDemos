0941029dde7ddf6a22356533700f7b525cd7e760
tlanders <tlanders@landersconsulting.biz>
2018-06-18 22:37:56 -0500

    finished sublists from item 47

--- src/main/java/effectivejava/chapter7/Sublists.java
+++ src/main/java/effectivejava/chapter7/Sublists.java
@@ -2,6 +2,7 @@ package effectivejava.chapter7;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Sublists {
@@ -11,10 +12,10 @@ public class Sublists {
 	}
 	
 	public static <E> Stream<List<E>> prefixes(List<E> list) {
-		return Stream.of(Collections.emptyList());
+		return IntStream.rangeClosed(0, list.size()).mapToObj(i -> list.subList(0, i));
 	}
 	
 	public static <E> Stream<List<E>> suffixes(List<E> list) {
-		return Stream.of(Collections.emptyList());
+		return IntStream.rangeClosed(0, list.size() - 1).mapToObj(i -> list.subList(i, list.size()));
 	}
 }
--- src/test/java/effectivejava/chapter7/SublistsTests.java
+++ src/test/java/effectivejava/chapter7/SublistsTests.java
@@ -13,12 +13,23 @@ public class SublistsTests {
 	public void testSublists() {
 		List<Integer> iList = new ArrayList<>();
 		iList.add(1);
+		iList.add(2);
 		iList.add(3);
 		
 		List<List<Integer>> iSublists = Sublists.of(iList).collect(Collectors.toList());
 		
-		assertEquals(4, iSublists.size());
+		System.out.println("subs: " + iSublists);
 		
-//		assertTrue(powerSet.get(5).contains(2L));
+		assertEquals(7, iSublists.size());
+		
+		for(int i = 1; i < iSublists.size(); i++) {
+			assertTrue(iSublists.get(i).size() > 0);
+		}
+		
+		assertTrue(iSublists.contains(Arrays.asList(1, 2)));
+		assertTrue(iSublists.contains(Arrays.asList(1, 2, 3)));
+		assertTrue(iSublists.contains(Arrays.asList(1)));
+		assertTrue(iSublists.contains(Arrays.asList(2, 3)));
+		assertTrue(iSublists.contains(Arrays.asList()));
 	}
 }
