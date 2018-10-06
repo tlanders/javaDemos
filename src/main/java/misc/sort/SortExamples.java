a41cd545845344ea0ac4906c79a8be0e2d566782
tlanders <tlanders@landersconsulting.biz>
2018-10-06 10:13:49 -0500

    added type parameters to sort method

--- src/main/java/misc/sort/SortExamples.java
+++ src/main/java/misc/sort/SortExamples.java
@@ -2,9 +2,7 @@ package misc.sort;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -39,11 +37,48 @@ public class SortExamples {
 //                .thenComparing(Comparator.reverseOrder()).reversed());
         words.sort(Comparator.comparing(String::length).reversed().thenComparing(Comparator.naturalOrder()));
 	}
-	
+
+	public static <K,V extends Comparable<? super V>> Map<K,V> sortMapByValue(Map<K,V> mapToSort) {
+//	public static Map<String,String> sortMapByValue(Map<String, String> mapToSort) {
+		return mapToSort.entrySet().stream()
+				.sorted(Map.Entry.comparingByValue())
+//				.collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue(), (e1,e2) -> e1, LinkedHashMap::new));
+//				.sorted((entry1, entry2) -> entry1.getValue().compareTo(entry2.getValue()))
+				.collect(LinkedHashMap::new, (map, entry) -> map.put(entry.getKey(), entry.getValue()), Map::putAll);
+	}
+
 	public static void main(String [] args) throws Exception {
 		System.out.println("main started");
 
-		if(args.length >= 1) {
+		Map<String,String> m = new HashMap<>();
+		m.put("1", "abc");
+		m.put("a", "23");
+		m.put("4", "GDJ");
+		m.put("gh", "834");
+		m.put("AB", "834");
+		m.put("3", "PCw");
+		m.put("SD", "kve");
+        m.put("Z", "23");
+
+		System.out.println("unsorted m=" + m);
+
+		Map<String,String> resultMap = sortMapByValue(m);
+
+		System.out.println("sorted=" + resultMap);
+
+		Map<String, Integer> m2 = new HashMap<>();
+        m2.put("1", 7);
+        m2.put("a", 54);
+        m2.put("4", 9);
+        m2.put("gh", 3);
+        m2.put("AB", 23);
+
+        System.out.println("unsorted m2=" + m2);
+        Map<String,Integer> resultMap2 = sortMapByValue(m2);
+
+        System.out.println("sorted=" + resultMap2);
+
+        if(args.length >= 1) {
 			List<String> words = Files.lines(Paths.get(args[0])).collect(Collectors.toList());	
 			List<String> words2 = new ArrayList<>(words);			
 			List<String> words3 = new ArrayList<>(words);
