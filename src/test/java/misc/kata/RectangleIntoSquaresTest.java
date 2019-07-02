f403e11436b624279c4b2b4f9c62586476c55000
tlanders <768713+tlanders@users.noreply.github.com>
2019-07-02 12:14:20 -0500

    rectangle into squares kata from codewars

---
+++ src/test/java/misc/kata/RectangleIntoSquaresTest.java
@@ -0,0 +1,31 @@
+package misc.kata;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static misc.kata.RectangleIntoSquares.sqInRect;
+import static org.junit.Assert.assertEquals;
+
+public class RectangleIntoSquaresTest {
+    @Test
+    public void testSqInRect() {
+        assertEquals(null, sqInRect(0, 0));
+        assertEquals(null, sqInRect(1, 1));
+        assertEquals(null, sqInRect(2, 2));
+
+        assertSqInRectangleResults(new int[]{1,1}, sqInRect(1, 2));
+        assertSqInRectangleResults(new int[]{1,1}, sqInRect(2, 1));
+        assertSqInRectangleResults(new int[]{1,1,1}, sqInRect(3, 1));
+        assertSqInRectangleResults(new int[]{2,1,1}, sqInRect(2, 3));
+        assertSqInRectangleResults(new int[]{3,2,1,1}, sqInRect(3, 5));
+        assertSqInRectangleResults(new int[]{3,2,1,1}, sqInRect(5, 3));
+//        assertSqInRectangleResults(new int[]{3,2,1,1}, sqInRect(1, 10000));
+    }
+
+    private void assertSqInRectangleResults(int[] expected, List<Integer> actual) {
+        for(int i = 0; i < expected.length; i++) {
+            assertEquals((Integer) expected[i], actual.get(i));
+        }
+    }
+}
