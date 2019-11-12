6a8dbc2e7a3d844670f9456deea883de773edb14
tlanders <tlanders@users.noreply.github.com>
2019-11-11 20:38:50 -0600

    added partition tests for up to 5 element lists

--- src/test/java/algorithms/sort/QuickSortTests.java
+++ src/test/java/algorithms/sort/QuickSortTests.java
@@ -29,6 +29,17 @@ public class QuickSortTests {
         Assert.assertArrayEquals(new Integer[]{1}, quickSortInt.partition(new Integer[]{1}, 0, 0));
         Assert.assertArrayEquals(new Integer[]{1,1}, quickSortInt.partition(new Integer[]{1,1}, 0, 1));
         Assert.assertArrayEquals(new Integer[]{1,2}, quickSortInt.partition(new Integer[]{1,2}, 0, 1));
+        Assert.assertArrayEquals(new Integer[]{1,2,3}, quickSortInt.partition(new Integer[]{1,2,3}, 0, 2));
         Assert.assertArrayEquals(new Integer[]{1,2,3}, quickSortInt.partition(new Integer[]{1,3,2}, 0, 2));
+        Assert.assertArrayEquals(new Integer[]{1,2,3}, quickSortInt.partition(new Integer[]{3, 1, 2}, 0, 2));
+        Assert.assertArrayEquals(new Integer[]{1,2,3}, quickSortInt.partition(new Integer[]{3, 2, 1}, 0, 2));
+        Assert.assertArrayEquals(new Integer[]{1,2,3,4}, quickSortInt.partition(new Integer[]{1,2,3,4}, 0, 3));
+        Assert.assertArrayEquals(new Integer[]{1,2,3,4}, quickSortInt.partition(new Integer[]{1,4,2,3}, 0, 3));
+        Assert.assertArrayEquals(new Integer[]{1,2,3,4}, quickSortInt.partition(new Integer[]{1,4,3,2}, 0, 3));
+        Assert.assertArrayEquals(new Integer[]{2,1,3,4}, quickSortInt.partition(new Integer[]{2,1,4,3}, 0, 3));
+        Assert.assertArrayEquals(new Integer[]{1,3,2,4}, quickSortInt.partition(new Integer[]{4,3,2,1}, 0, 3));
+        Assert.assertArrayEquals(new Integer[]{1,2,3,4,5}, quickSortInt.partition(new Integer[]{1,2,3,4,5}, 0, 4));
+        Assert.assertArrayEquals(new Integer[]{2,1,3,5,4}, quickSortInt.partition(new Integer[]{2,4,1,5,3}, 0, 4));
+        Assert.assertArrayEquals(new Integer[]{1,4,3,2,5}, quickSortInt.partition(new Integer[]{5,4,3,2,1}, 0, 4));
     }
 }
